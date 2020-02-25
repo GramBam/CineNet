07a76330e42b200d096df8fd2f3796ccdcef5f6e
Graham <graham.moss@hotmail.com>
2020-02-25 10:46:58 -0500

    start likes & comments routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -94,4 +94,81 @@ router.delete('/:id', async(req, res) => {
   }
 });
 
+// @route   PUT api/posts/like/:id
+// @desc    Like a post
+// @access  Private
+router.put('/like/:id', auth, async(req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if(posts.likes.filter(like => like.user.toString() === req.user.id)) {
+      return res.status(400).json({msg: 'Post already liked'});
+    }
+
+    post.likes.unshift({user: req.user.id});
+
+    await post.save();
+
+    res.json(post.likes);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   PUT api/posts/unlike/:id
+// @desc    Like a post
+// @access  Private
+router.put('/like/:id', auth, async(req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if(posts.likes.filter(like => like.user.toString() === req.user.id)) {
+      return res.status(400).json({msg: 'Post has not yet been liked'});
+    }
+
+    const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.likes);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   PUT api/posts/comment/:id
+// @desc    Comment on a post
+// @access  Private
+
+router.post('/comment/:id', [auth, [check('text', 'Text is r equired').not().isEmpty()]], async(req,res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()});
+  }
+
+  try {
+
+  } catch (err) {
+    const user = await User.findById(req.user.id).select('-password');
+    const post = await Post.findById(req.params.id);
+
+    const newComment = {
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    }
+
+    post.comments.unshift(newComment);
+
+    await post.save();
+
+    res.json(post.comments);
+  }
+})
+
 module.exports = router;
