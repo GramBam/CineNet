a43b7a24b4ed4a82160deda785a9a6e7948694e7
Graham <graham.moss@hotmail.com>
2020-05-29 20:47:55 -0400

    register action, cleanup

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(express.json({extended: false}));
-app.get('/', (req,res) => res.send('API Running'));
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use(express.json({ extended: false }));
+app.get('/', (req, res) => res.send('API Running'));
+app.use('/api/users', require('./config/routes/api/users'));
+app.use('/api/auth', require('./config/routes/api/auth'));
+app.use('/api/profile', require('./config/routes/api/profile'));
+app.use('/api/posts', require('./config/routes/api/posts'));
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
